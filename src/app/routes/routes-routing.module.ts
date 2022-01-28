fef37f30ff03489577636b2cbbdc5c4666aed46f
dengkai <dengkai@wxchina.com>
2022-01-29 07:29:44 +0800

    1 删除绝对路径

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {IndexComponent} from "./index/index.component";
-import {BlogComponent} from './blog/blog.component'
+import {BlogComponent} from './blog/blog.component';
+ import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,9 @@ const routes: Routes = [
   },
   {
     path: 'blog', component: BlogComponent, data: {title: 'Blog', name: 'Blog'}
+  },
+  {
+    path: 'about', component: AboutComponent, data: {title: 'About' , name: 'About'}
   }
 ];
 
