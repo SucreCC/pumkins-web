d1a6aaded2b83400203ea425e250e8717a8b3a5c
dengkai <dengkai@wxchina.com>
2022-01-28 12:47:15 +0800

    1 添加主页

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -7,6 +7,27 @@ import { WelcomeComponent } from "./welcome/welcome.component";
 import { IndexComponent } from "./index/index.component";
 
 
+import { CommonModule } from '@angular/common';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BlogComponent } from './index/blog/blog.component';
+import { AboutComponent } from './index/about/about.component';
+import { BlogDetailComponent } from './index/blog/blog-detail/blog-detail.component';
+
+import { ServiceblogService } from './index/blog/blog-service.service';
+import { RelayOnComponent } from './index/about/About-Components/relay-on/relay-on.component';
+import { TopContentComponent } from './index/about/About-Components/top-content/top-content.component';
+
+
+import { BannerComponent } from './index/shared/banner/banner.component';
+import { BannerNavigationComponent } from './index/shared/banner-navigation/banner-navigation.component';
+import { FooterComponent } from './index/shared/footer/footer.component';
+import {BannerContentComponent} from "./index/shared/banner-content/banner-content.component";
+
+
 const COMPONENT =[
   WelcomeComponent,
   IndexComponent
@@ -20,13 +41,31 @@ const COMPONENTS_NOROUNT = [
 @NgModule({
   declarations: [
     COMPONENT,
-    IndexComponent
+
+
+    IndexComponent,
+    BlogComponent,
+    AboutComponent,
+    BlogDetailComponent,
+    RelayOnComponent,
+    TopContentComponent,
+    BannerComponent,
+    BannerContentComponent,
+    BannerNavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    RoutesRoutingModule
+    RoutesRoutingModule,
+
+
+    CommonModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ServiceblogService],
   exports: [
     WelcomeComponent
   ],
