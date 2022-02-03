2b8074a712083168af23d8c59defb764e45f5820
dengkai <dengkai@wxchina.com>
2022-02-03 10:46:27 +0800

    1 引入vditor

--- src/app/routes/blog/blog-write/blog-write.component.ts
+++ src/app/routes/blog/blog-write/blog-write.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {EditorConfig} from "../../../share/component/editor/editor-config/editor-config";
 import {ServiceblogService} from "../data/blog-service.service";
 
+import Vditor from 'vditor';
+
 @Component({
   selector: 'app--blog-write',
   templateUrl: './blog-write.component.html',
@@ -9,25 +11,49 @@ import {ServiceblogService} from "../data/blog-service.service";
 })
 export class BlogWriteComponent implements OnInit {
 
-  constructor(public service: ServiceblogService) {
-    this.service.showEdit = false;
-    this.config = new EditorConfig({height: 'calc(100vh - 71px)'});
-    this.markdown = '测试内容';
+  // constructor(public service: ServiceblogService) {
+  //   this.service.showEdit = false;
+  //   this.config = new EditorConfig({height: 'calc(100vh - 71px)'});
+  //   this.markdown = '测试内容';
+  //
+  // }
+  //
+  // ngOnInit(): void {
+  // }
+  //
+  // /**
+  //  * editor的配置参数信息
+  //  */
+  // config: EditorConfig;
+  //
+  // /**
+  //  * markdown的内容
+  //  */
+  // markdown: string;
 
-  }
+
+  vditor: Vditor;
 
   ngOnInit(): void {
+    this.vditor = new Vditor('vditor', {
+      mode:'sv',
+      height: 'auto',
+      icon:'material',
+      theme:'classic',
+      toolbarConfig: {
+        pin: true,
+      },
+      cache: {
+        enable: false,
+      },
+      after: () => {
+        this.vditor.setValue('Hello, Vditor + Angular!');
+      }
+    });
   }
 
-  /**
-   * editor的配置参数信息
-   */
-  config: EditorConfig;
-
-  /**
-   * markdown的内容
-   */
-  markdown: string;
-
+  coverToJson(){
+    this.vditor.exportJSON("")
+  }
 
 }
