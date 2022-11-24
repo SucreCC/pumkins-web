7d58aab5931a39af7fa6ccdd98e3f866d3adc8a7
dengkai <dengkai@wxchina.com>
2022-11-24 17:00:49 +0800

    个人中心

--- src/app/routes/backend/backend.module.ts
+++ src/app/routes/backend/backend.module.ts
@@ -7,6 +7,7 @@ import {UserCenterComponent} from "./user-center/user-center.component";
 import {BackendManageComponent} from "./backend-manage/backend-manage.component";
 import {IndexComponent} from "./index.component";
 import {AddBlogComponent} from "./add-blog/add-blog.component";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 const COMPONENT = [
@@ -30,9 +31,10 @@ const COMPONENTS_NOROUNT = []
     declarations: [
         ...COMPONENT,
     ],
-    imports: [
-        ...COMPONENT_IMPORT,
-    ],
+  imports: [
+    ...COMPONENT_IMPORT,
+    NzButtonModule,
+  ],
     providers: [],
     exports: []
 })
