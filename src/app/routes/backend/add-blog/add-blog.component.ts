83438a1b25cea229283aa9e26dcdee5b8947d642
dengkai <dengkai@wxchina.com>
2022-11-28 17:02:40 +0800

    补充文章信息

--- src/app/routes/backend/add-blog/add-blog.component.ts
+++ src/app/routes/backend/add-blog/add-blog.component.ts
@@ -1,10 +1,139 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import Vditor from "vditor";
+import {NzUploadFile} from "ng-zorro-antd/upload";
+
+const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+
+
 @Component({
   selector: 'app-add-blog',
   templateUrl: './add-blog.component.html',
   styleUrls: ['./add-blog.component.less']
 })
-export class AddBlogComponent{
 
-  constructor() {}
+export class AddBlogComponent implements OnInit {
+
+  constructor() {
+  }
+
+  vditor: Vditor;
+  // vditor 初始化时的配置
+  option: IOptions = {
+    mode: 'sv',
+    height: '900px',
+    theme: 'classic',
+    toolbarConfig: {
+      pin: true,
+    },
+    lang: 'en_US',
+
+    preview: {
+      markdown: {
+        autoSpace: true,
+        toc: true,
+        mark: true,
+      }
+    },
+
+    after: () => {
+      this.vditor.setValue(this.content);
+    },
+    input(md) {
+      localStorage.setItem("oldMarkdown", md);
+    },
+  };
+
+  visible: boolean = false;
+
+  blog: {
+    name: ''
+  }
+
+  title: string = '';
+  cover: string[] = [];
+  radioValue = 'defaultCover';
+  fileList: NzUploadFile[] = [];
+  description:'';
+  addCategory: any;
+  category: any;
+
+
+
+  previewImage: string | undefined = '';
+  previewVisible = false;
+  content: any = '';
+
+  handlePreview = async (file: NzUploadFile): Promise<void> => {
+    if (!file.url && !file['preview']) {
+      file['preview'] = await getBase64(file.originFileObj!);
+    }
+    this.previewImage = file.url || file['preview'];
+    this.previewVisible = true;
+  };
+
+  ngOnInit(): void {
+    this.vditor = new Vditor('vditor', this.option);
+  }
+
+
+  close() {
+    this.visible = false;
+  }
+
+  open() {
+    this.visible = true;
+  }
+
+  change() {
+    console.log(this.radioValue)
+  }
+
+
+  // tags
+  tags = ['Unremovable', 'Tag 2', 'Tag 3'];
+  inputVisible = false;
+  inputValue = '';
+  @ViewChild('inputElement', {static: false}) inputElement?: ElementRef;
+  isVisitable: boolean = false;
+
+  handleClose(removedTag: {}): void {
+    this.tags = this.tags.filter(tag => tag !== removedTag);
+  }
+
+  sliceTagName(tag: string): string {
+    const isLongTag = tag.length > 20;
+    return isLongTag ? `${tag.slice(0, 20)}...` : tag;
+  }
+
+  showInput(): void {
+    this.inputVisible = true;
+    setTimeout(() => {
+      this.inputElement?.nativeElement.focus();
+    }, 10);
+  }
+
+  handleInputConfirm(): void {
+    console.log(this.inputValue)
+    if (this.inputValue && this.tags.indexOf(this.inputValue) === -1) {
+      this.tags = [...this.tags, this.inputValue];
+    }
+    this.inputValue = '';
+    this.inputVisible = false;
+  }
+
+//  Category
+  listOfGroupOption = [
+    { label: 'Jack', value: 'jack', groupLabel: 'Manager' },
+    { label: 'Lucy', value: 'lucy', groupLabel: 'Manager' },
+    { label: 'Tom', value: 'tom', groupLabel: 'Engineer' }
+  ];
+
 }
+
+
--- src/app/routes/backend/backend.module.ts
+++ src/app/routes/backend/backend.module.ts
@@ -11,6 +11,18 @@ import {NzButtonModule} from "ng-zorro-antd/button";
 import {FavouriteBlogComponent} from "./favourite-blog/favourite-blog.component";
 import {BlogArticleComponent} from "../layout/home/blog-article/blog-article.component";
 import {HomeModule} from "../layout/home/home.module";
+import {NzDrawerModule} from "ng-zorro-antd/drawer";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzDatePickerModule} from "ng-zorro-antd/date-picker";
+import {NzRadioModule} from "ng-zorro-antd/radio";
+import {NzUploadModule} from "ng-zorro-antd/upload";
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzModalModule} from "ng-zorro-antd/modal";
+import {NzNoAnimationModule} from "ng-zorro-antd/core/no-animation";
+import {NzTagModule} from "ng-zorro-antd/tag";
+import {NzSwitchModule} from "ng-zorro-antd/switch";
 
 
 const COMPONENT = [
@@ -39,6 +51,18 @@ const COMPONENTS_NOROUNT = []
     ...COMPONENT_IMPORT,
     NzButtonModule,
     HomeModule,
+    NzDrawerModule,
+    NzFormModule,
+    NzSelectModule,
+    NzInputModule,
+    NzDatePickerModule,
+    NzRadioModule,
+    NzUploadModule,
+    NzIconModule,
+    NzModalModule,
+    NzNoAnimationModule,
+    NzTagModule,
+    NzSwitchModule,
   ],
     providers: [],
     exports: []
