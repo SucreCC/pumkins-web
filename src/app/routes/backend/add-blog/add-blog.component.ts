7993d651aaa15b3e92f0acd0e890a44424bc6968
dengkai <dengkai@wxchina.com>
2023-01-19 16:04:00 +0800

    save category

--- src/app/routes/backend/add-blog/add-blog.component.ts
+++ src/app/routes/backend/add-blog/add-blog.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import Vditor from "vditor";
 import {NzUploadFile} from "ng-zorro-antd/upload";
 import {_HttpClient, SettingsService} from "@delon/theme";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
   new Promise((resolve, reject) => {
@@ -17,7 +18,7 @@ export class Blog {
   title: string = '';
   tags: string[];
   markdown: string;
-  category: number;
+  categoryId: number;
   description: string = '';
   username: string = '';
   isVisible: boolean;
@@ -34,12 +35,14 @@ export class Blog {
 export class AddBlogComponent implements OnInit {
 
   constructor(private settingService: SettingsService,
-              private http: _HttpClient,) {
+              private http: _HttpClient,
+              private message: NzMessageService,) {
   }
 
   saveBlogUrl: string = "/blog/save-blog"
   saveTagsUrl: string = "/blog/save-tags"
   saveCategoryUrl: string = "/blog/save-category"
+  getCategoryUrl: string = "/blog/get-category"
 
   vditor: Vditor;
   // vditor 初始化时的配置
@@ -76,22 +79,15 @@ export class AddBlogComponent implements OnInit {
   fileList: NzUploadFile[] = [];
   description: '';
   addCategory: any;
-  category: number;
-
+  categoryValue: number;
+  categoryId: number;
   previewImage: string | undefined = '';
   previewVisible = false;
   content: any = '';
-
-  listOfGroupOption = [
-    {label: 'Jack', value: 0},
-    {label: 'Lucy', value: 1},
-    {label: 'Tom', value: 2}
-  ];
-
+  listOfGroupOption = [];
   isVisible: boolean = true;
   workOrLife: boolean = true;
   isDraft: boolean = false;
-
 
   handlePreview = async (file: NzUploadFile): Promise<void> => {
     if (!file.url && !file['preview']) {
@@ -105,22 +101,38 @@ export class AddBlogComponent implements OnInit {
     this.vditor = new Vditor('vditor', this.option);
   }
 
-
-  close() {
+  cancel() {
     this.visible = false;
-    // this.saveTags();
+  }
+
+  // @ts-ignore
+  close() {
+    this.saveTags();
     this.saveCategory();
-    // this.buildBlog();
-    // this.saveBlog();
+    this.buildBlog();
+
+    if (this.title === "") {
+      this.message.warning("please input the title");
+      return false;
+    }
+
+    if (!this.categoryValue) {
+      this.message.warning("please choose the category");
+      return false;
+    }
+    this.visible = false;
+    this.saveBlog();
   }
 
   saveCategory() {
     for (const value of this.listOfGroupOption.values()) {
-      if (value.value === this.category) {
-        let category = {label: value.label, value: this.category}
+      // @ts-ignore
+      if (value.value === this.categoryValue) {
+        // @ts-ignore
+        let category = {label: value.label, value: this.categoryValue}
         this.http.post(this.saveCategoryUrl, category).subscribe(resp => {
           if (resp.status === 0) {
-            console.log(resp.data);
+            this.categoryId = resp.data.id;
           }
         })
       }
@@ -154,7 +166,7 @@ export class AddBlogComponent implements OnInit {
     }
 
     this.blog.tags = this.tags;
-    this.blog.category = this.category;
+    this.blog.categoryId = this.categoryId;
     this.blog.description = this.description;
     // @ts-ignore
     this.blog.username = this.settingService.getUser().username;
@@ -165,13 +177,19 @@ export class AddBlogComponent implements OnInit {
 
 
   open() {
+    this.getCategoryList();
     this.visible = true;
   }
 
-  change() {
-    console.log(this.radioValue)
-  }
+  getCategoryList() {
+    this.http.get(this.getCategoryUrl).subscribe(resp => {
+      if (resp.status === 0) {
+        this.listOfGroupOption = resp.data;
+        console.log(this.listOfGroupOption);
 
+      }
+    })
+  }
 
   // tags
   tags = [];
@@ -206,8 +224,11 @@ export class AddBlogComponent implements OnInit {
   }
 
   addNewCategory(): void {
-    let newCategory: any = {label: this.addCategory, value: this.listOfGroupOption.length}
-    this.listOfGroupOption = [...this.listOfGroupOption, newCategory]
+    if(this.listOfGroupOption!=null){
+      let newCategory: any = {label: this.addCategory, value: this.listOfGroupOption.length}
+      // @ts-ignore
+      this.listOfGroupOption = [...this.listOfGroupOption, newCategory]
+    }
   }
 }
 
